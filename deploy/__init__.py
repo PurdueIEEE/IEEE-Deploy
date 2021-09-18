c4104de1c9e59dc005c52e0a6201837cfffc9154
Hadi Ahmed <hadiahmed098@gmail.com>
2021-09-17 23:33:14 -0400

    Added license and error codes (closes #1)

--- deploy/__init__.py
+++ deploy/__init__.py
@@ -1,4 +1,18 @@
-#!/usr/bin/env python3
+'''
+Copyright 2021 Hadi Ahmed
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
 
 from flask import Flask, request
 import logging, datetime, hmac, hashlib, subprocess
@@ -29,7 +43,7 @@ def deploy():
             signature = hmac.new(secrets.token, request.data, hashlib.sha256).hexdigest()
             if signature != request.headers["X-Hub-Signature-256"][7:]:
                 logger.warn('%s -- %s -- Invalid GitHub WebHook Signature' % (time_recv, request.remote_addr))
-                return ''
+                return '', 403 # Forbidden
             
             body = request.json
             logger.info('%s -- %s -- Recieved GitHub WebHook %s for repo %s' % (time_recv, request.remote_addr, request.headers['X-GitHub-Delivery'], body['repository']['full_name']))
@@ -47,16 +61,16 @@ def deploy():
                     good=False
                 finally:
                     # Spit a response back
-                    return '<p>Recieved push to %s, %s<p>' % (body['repository']['full_name'], "Succeed to git pull" if good else "Failed to git pull")
+                    return '<p>Recieved push to %s, %s<p>' % (body['repository']['full_name'], "Succeed to git pull" if good else "Failed to git pull"), 200 if good else 500 # Good or server error
             else:
                 # Not in mapping table
                 logger.warn('%s -- %s -- Repository %s is not included in mapping table' % (time_recv, request.remote_addr, body['repository']['full_name']))
-                return '<p>Recieved push to %s, not in mapping table</p>' % (body['repository']['full_name'])
+                return '<p>Recieved push to %s, not in mapping table</p>' % (body['repository']['full_name']), 400 # Bad Request
 
         else: # Not a valid WebHook
             logger.info('%s -- %s -- Not a GitHub WebHook or Improper Headers' % (time_recv, request.remote_addr))
-            return ''
+            return '', 404 # Not Found
     else:
         # This branch should never be hit, but just in case
         logger.error('%s -- %s -- Flask allowed non-POST request' % (time_recv, request.remote_addr))
-        return ''
+        return '', 404 # Not Found
